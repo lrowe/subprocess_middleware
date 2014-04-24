1edab5f7ec420a88c707213a309b5de921f5d49e
Laurence Rowe <laurence@lrowe.co.uk>
2014-04-24 15:12:07 -0700

    tox

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@ import os.path
 from setuptools import setup, find_packages
 
 here = os.path.dirname(__file__)
-README = open(os.path.join(here, 'README.rst')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+try:
+    with open(os.path.join(here, 'README.txt')) as f:
+        README = f.read()
+    with open(os.path.join(here, 'CHANGES.txt')) as f:
+        CHANGES = f.read()
+except IOError:
+    README = CHANGES = ''  # tox
 
 requires = [
     'WebOb',
